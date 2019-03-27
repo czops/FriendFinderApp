2b90f4f413278cbb9ca608afdfc0f2cf4c41dc9e
czops <37029225+czops@users.noreply.github.com>
2019-03-27 17:40:47 -0500

    finished most of structure

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -6,9 +6,10 @@ var path = require("path");
 //define here to avoid scope issues
 var friends = [
     {
-        name: "obiwankenobi",
+        route: "obiwankenobi",
+        name: "Obi Wan Kenobi",
         photo: "https://cdn.vox-cdn.com/thumbor/YP3HK6voEMeWojSG9aGdJQMYlYY=/0x0:1300x650/1200x800/filters:focal(537x203:745x411)/cdn.vox-cdn.com/uploads/chorus_image/image/56242795/Obi_Wan.0.jpg",
-        scores: [3,3,4,5,1,2,1,3,4,5]
+        scores: [3, 3, 4, 5, 1, 2, 1, 3, 4, 5]
     },
 ];
 
@@ -17,7 +18,7 @@ module.exports = function (app) {
     // API GET Requests
     // Below code handles when users "visit" a page.
     // In each of the below cases when a user visits a link
-    
+
     app.get("/api/friends", function (req, res) {
         return res.json(friends);
     });
@@ -30,19 +31,30 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
         // req.body hosts is equal to the JSON post sent from the user
         // This works because of our body parsing middleware
-        var newFriend = req.body;
 
-            // Using a RegEx Pattern to remove spaces from newCharacter
-            // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-            // newCustomer.routeName = newCustomer.name.replace(/\s+/g, "").toLowerCase();
+        // Using a RegEx Pattern to remove spaces from newCharacter
+        var routeName = newFriend.replace(/\s+/g, "").toLowerCase();
+        console.log("route: " + routeName);
+        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+        // newCustomer.routeName = newCustomer.name.replace(/\s+/g, "").toLowerCase();
 
-            console.log(newFriend.name + "Has submitted their survey!");
+        //can get rid of this since the body is assigned below in the new variable
+        // var newFriend = req.body;
 
-            friends.push(newFriend);
+        //previous examples have used a constructor, but you don't have this setup
+        var newFriend = {
+            route: routeName,
+            name: req.body.name,
+            photo: req.body.photo
+        };
+        //not sure what this will do if I just keep it here...
+    res.status(204).end();
+    console.log(newFriend.name + " has submitted their survey!");
 
-            res.json(newFriend);
+    friends.push(newFriend);
 
+    res.json(newFriend);
 
-    });
+});
 
 };
