3186efe1b7b8e8afd6c4eed592fe3ad161b82560
czops <37029225+czops@users.noreply.github.com>
2019-04-03 18:08:19 -0500

    pre heroku upload commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,65 @@
+// // ===============================================================================
+// // ROUTING
+// // ===============================================================================
+// var path = require("path");
+
+// //define here to avoid scope issues
+// var friends = [
+//     {
+//         name: "obiwankenobi",
+//         photo: " ",
+//         scores: []
+//     },
+// ];
+
+// module.exports = function (app) {
+
+//     // API GET Requests
+//     // Below code handles when users "visit" a page.
+//     // In each of the below cases when a user visits a link
+//     // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+//     // ---------------------------------------------------------------------------
+//     // Routes you to the JSON contained in the express server for the reserved tables
+
+//     app.get("/api/friends", function (req, res) {
+//         return res.json(friends);
+//     });
+
+//     // Create New Characters - takes in JSON input
+//     app.post("/api/", function (req, res) {
+//         // req.body hosts is equal to the JSON post sent from the user
+//         // This works because of our body parsing middleware
+//         var newCustomer = req.body;
+
+//         if (customers.length < 5) {
+//             // Using a RegEx Pattern to remove spaces from newCharacter
+//             // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//             // newCustomer.routeName = newCustomer.name.replace(/\s+/g, "").toLowerCase();
+
+//             console.log(newCustomer.name + "is seated at a table.");
+
+//             customers.push(newCustomer);
+
+//             res.json(newCustomer);
+
+//         } else if (customers.length > 4) {
+//             console.log(newCustomer.name + "is on the waitlist");
+
+//             waitlist.push(newCustomer);
+
+//             res.json(newCustomer);
+//         }
+
+//     });
+
+// };
+
+
+
+
+///.........................
+
+
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -6,9 +68,10 @@ var path = require("path");
 //define here to avoid scope issues
 var friends = [
     {
-        name: "obiwankenobi",
-        photo: " ",
-        scores: []
+        route: "obiwankenobi",
+        name: "Obi Wan Kenobi",
+        photo: "https://cdn.vox-cdn.com/thumbor/YP3HK6voEMeWojSG9aGdJQMYlYY=/0x0:1300x650/1200x800/filters:focal(537x203:745x411)/cdn.vox-cdn.com/uploads/chorus_image/image/56242795/Obi_Wan.0.jpg",
+        scores: [3, 3, 4, 5, 1, 2, 1, 3, 4, 5]
     },
 ];
 
@@ -17,39 +80,44 @@ module.exports = function (app) {
     // API GET Requests
     // Below code handles when users "visit" a page.
     // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-    // Routes you to the JSON contained in the express server for the reserved tables
 
     app.get("/api/friends", function (req, res) {
         return res.json(friends);
     });
 
-    // Create New Characters - takes in JSON input
-    app.post("/api/", function (req, res) {
+    app.get("/api/survey", function (req, res) {
+        return res.json(friends);
+    });
+
+    // Create New Friend- takes in JSON input
+    app.post("/api/friends", function (req, res) {
         // req.body hosts is equal to the JSON post sent from the user
         // This works because of our body parsing middleware
-        var newCustomer = req.body;
 
-        if (customers.length < 5) {
-            // Using a RegEx Pattern to remove spaces from newCharacter
-            // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-            // newCustomer.routeName = newCustomer.name.replace(/\s+/g, "").toLowerCase();
+        // Using a RegEx Pattern to remove spaces from newCharacter
+        var routeName = newFriend;
+        //.replace(/\s+/g, "").toLowerCase();
+        console.log("route: " + routeName);
+        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+        // newCustomer.routeName = newCustomer.name.replace(/\s+/g, "").toLowerCase();
 
-            console.log(newCustomer.name + "is seated at a table.");
+        //can get rid of this since the body is assigned below in the new variable
+        // var newFriend = req.body;
 
-            customers.push(newCustomer);
+        //previous examples have used a constructor, but you don't have this setup
+        var newFriend = {
+            route: routeName,
+            name: req.body.name,
+            photo: req.body.photo
+        };
+        //not sure what this will do if I just keep it here...
+    res.status(204).end();
+    console.log(newFriend.name + " has submitted their survey!");
 
-            res.json(newCustomer);
+    friends.push(newFriend);
 
-        } else if (customers.length > 4) {
-            console.log(newCustomer.name + "is on the waitlist");
+    res.json(newFriend);
 
-            waitlist.push(newCustomer);
-
-            res.json(newCustomer);
-        }
-
-    });
+});
 
 };
